becc1ef0dcb2b5e7b7af0477d799e78a27329c64
pavels-s <pavels.sarkels@gmail.com>
2022-08-16 11:42:37 +0300

    Invoke, checkboxes and radiobuttons

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -1,5 +1,8 @@
 /// <reference types="cypress" />
 
+//npm start
+//npx cypress open
+
 describe('Our first suite', () => {
 
 
@@ -135,7 +138,59 @@ describe('Our first suite', () => {
 
     })
 
-    it.only
+    it('assert property', () => {
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Datepicker').click()
+
+        cy.contains('nb-card', 'Common Datepicker').find('input').then( input => {
+            //input is jquerry element, so we need first to wrap it
+            cy.wrap(input).click()
+            cy.get('nb-calendar-day-picker').contains('17').click()
+            //requesting dev tool properties at right side -> value (line with chosen date)
+            cy.wrap(input).invoke('prop', 'value').should('contain', 'Aug 17, 2022')
+        })
+    })
+
+
+    it('radio button', () => {
+
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        cy.contains('nb-card', 'Using the Grid').find('[type="radio"]').then( radioButtons => {
+            cy.wrap(radioButtons)
+                .first()
+                .check({force: true})
+
+            cy.wrap(radioButtons)
+                //finding by index - equal 1
+                .eq(1)
+                .check({force: true})
+
+            cy.wrap(radioButtons)
+                .first()
+                .should('not.be.checked')
+
+            cy.wrap(radioButtons)
+                .eq(2)
+                .should('be.disabled')
+        })
+
+    })
+
+
+    it.only('check boxes', () => {
+        cy.visit('/')
+        cy.contains('Modal & Overlays').click()
+        cy.contains('Toastr').click()
+
+        //cy.get('[type="checkbox"]').check({force: true})
+        cy.get('[type="checkbox"]').eq(0).click({force: true})
+        cy.get('[type="checkbox"]').eq(1).check({force: true})
+
+    })
 
 
 })
