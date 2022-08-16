20fd553832eda65c799e9ca7b6fa8bd65fec8910
pavels-s <pavels.sarkels@gmail.com>
2022-08-16 15:14:08 +0300

    PopUps and Tooltips

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -230,7 +230,7 @@ describe('Our first suite', () => {
     })
 
 
-    it.only('web tables', () => {
+    it('web tables', () => {
         cy.visit('/')
         cy.contains('Tables & Data').click()
         cy.contains('Smart Table').click()
@@ -265,6 +265,40 @@ describe('Our first suite', () => {
             cy.wrap(tableRow).find('td').eq(6).should('contain', 20)
         })
 
+
+    })
+
+
+    it('tooltips', () => {
+        cy.visit('/')
+        cy.contains('Modal & Overlays').click()
+        cy.contains('Tooltip').click()
+
+        cy.contains('nb-card', 'Colored Tooltips')
+            .contains('Default').click()
+        
+            cy.get('nb-tooltip').should('contain', 'This is a tooltip')
+    
+    })
+
+
+    it.only('tooltips', () => {
+        cy.visit('/')
+        cy.contains('Tables & Data').click()
+        cy.contains('Smart Table').click()
+
+        //#1
+        cy.get('tbody tr').first().find('.nb-trash').click()
+        cy.on('window:confirm', (confirm) => {
+            expect(confirm).to.equal('Are you sure you want to delete?')
+        })
+
+        //#2
+        const stub = cy.stub()
+        cy.on('window:confirm', stub)
+        cy.get('tbody tr').first().find('.nb-trash').click().then(() => {
+            expect(stub.getCall(0)).to.be.calledWith('Are you sure you want to delete?')
+        })
 
     })
 
