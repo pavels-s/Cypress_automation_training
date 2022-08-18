c9d1b07b64da943308bfdb3cc3c67f103f88fc02
pavels-s <pavels.sarkels@gmail.com>
2022-08-18 15:11:20 +0300

    Adding global method to commands.js

--- cypress/integration/testWithPageObjects.js
+++ cypress/integration/testWithPageObjects.js
@@ -6,7 +6,7 @@ import { onSmartTablePage } from "../support/page_objects/smartTablePage"
 describe('Test With Page Objects', () => {
 
     beforeEach('open application', () => {
-        cy.visit('/')
+        cy.openHomePage()
     })
 
     it('verify navigations across the table', () => {
